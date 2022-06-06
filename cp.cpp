88f54a478f888f6223698746fd47b05f8f32d610
anshc1 <anshchaturvadi2000@gmail.com>
2022-06-07 00:07:00 +0530

    upd

---
+++ cp.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+#define int long long int
+#define Maxx 10000000000000
+#define pb push_back
+#define v vector
+#define vi vector<int>
+#define rep(i,a,b) for(int i=a; i<=b; i++)
+#ifndef ONLINE_JUDGE
+#define dbg(x) cerr<<#x<< " "; _print(x);cerr<<endl; 
+#else
+#define dbg(x)
+#endif
+using namespace std;
+
+
+void _print(int x ){
+    cerr<<x<<" "; 
+}void _print(float x ){
+    cerr<<x<<" "; 
+}void _print(double x ){
+    cerr<<x<<" "; 
+}void _print(char x ){
+    cerr<<x<<" "; 
+}void _print(string x ){
+    cerr<<x<<" "; 
+}void _print(bool x){
+    cerr<<x<<" "; 
+}
+template<class T,class V> void _print(pair<T,V>x){
+    cerr<<"{"; _print(x.first);cerr<<",";_print(x.second); cerr<<"}"; 
+}
+template<class T> void _print(vector<T> x){
+    cerr<<"[ "; 
+    for(T i : x){
+        _print(i); 
+        cerr << " "; 
+    }
+    cerr<<"]"; 
+}
+template<class T> void _print(set<T> x){
+    cerr<<"[ "; 
+    for(T i : x){
+        _print(i); 
+        cerr << " "; 
+    }
+    cerr<<"]"; 
+}
+template<class T> void _print(multiset<T>x){
+    cerr<<"[ "; 
+    for(T i : x){
+        _print(i); 
+        cerr << " "; 
+    }
+    cerr<<"]"; 
+}
+template<class T , class V> void _print(map<T, V> x){
+    cerr<<"[ "; 
+    for(auto i : x){
+        _print(i); 
+        cerr<<" "; 
+    }
+    cerr<<" ]"; 
+}
+
+void solve()
+{
+    
+}
+signed main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+    freopen("inputf.in" , "r" , stdin ); 
+    freopen("outputf.out", "w" , stdout); 
+    freopen("err.txt","w",stderr);
+    #endif
+    clock_t start, end;
+    start = clock();
+    int t;
+    cin >> t;
+    //t=1;
+    while (t--)
+    {
+        solve();
+    }
+    end = clock();
+    double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
+    cerr << "Time taken by program is : " << fixed 
+         << time_taken << setprecision(5);
+    cerr << " sec " << endl;
+    return 0;
+}
+
---
+++ cp2.cpp
@@ -0,0 +1,125 @@
+#include <bits/stdc++.h>
+//#define int long long int
+#define Maxx 10000000000000
+#define pb push_back
+#define v vector
+#define vi vector<int>
+#define rep(i,a,b) for(int i=a; i<=b; i++)
+#ifndef ONLINE_JUDGE
+#define dbg(x) cerr<<#x<< " "; _print(x);cerr<<endl; 
+#else
+#define dbg(x)
+#endif
+using namespace std;
+
+
+void _print(int x ){
+    cerr<<x<<" "; 
+}void _print(float x ){
+    cerr<<x<<" "; 
+}void _print(double x ){
+    cerr<<x<<" "; 
+}void _print(char x ){
+    cerr<<x<<" "; 
+}void _print(string x ){
+    cerr<<x<<" "; 
+}void _print(bool x){
+    cerr<<x<<" "; 
+}
+template<class T,class V> void _print(pair<T,V>x){
+    cerr<<"{"; _print(x.first);cerr<<",";_print(x.second); cerr<<"}"; 
+}
+template<class T> void _print(vector<T> x){
+    cerr<<"[ "; 
+    for(T i : x){
+        _print(i); 
+        cerr << " "; 
+    }
+    cerr<<"]"; 
+}
+template<class T> void _print(set<T> x){
+    cerr<<"[ "; 
+    for(T i : x){
+        _print(i); 
+        cerr << " "; 
+    }
+    cerr<<"]"; 
+}
+template<class T> void _print(multiset<T>x){
+    cerr<<"[ "; 
+    for(T i : x){
+        _print(i); 
+        cerr << " "; 
+    }
+    cerr<<"]"; 
+}
+template<class T , class V> void _print(map<T, V> x){
+    cerr<<"[ "; 
+    for(auto i : x){
+        _print(i); 
+        cerr<<" "; 
+    }
+    cerr<<" ]"; 
+}
+void solve()
+{
+    int n ; 
+    cin >> n ; 
+    int k ; 
+    cin >> k  ;
+    vi a(n);  
+    rep(i ,0 , n-1){
+        cin >>a[i]; 
+    }
+    vi b =a; 
+    multiset<int>t; 
+    rep(i ,0 ,k-1){
+        for (int j = i; j < n; j += k)
+        {
+            t.insert(a[j]); 
+        }  
+        for (int j = i; j < n; j += k)
+        {
+            a[j] =*t.begin(); 
+            t.erase(t.begin()); 
+        }
+    }
+    bool ok =0 ;
+    sort(b.begin() ,b.end());  
+    rep(i ,0 ,n-1){
+        if(a[i]!= b[i]){
+            ok =1; 
+        }
+    }
+    if(!ok){
+        cout<<"Yes\n"; 
+    }else{
+        cout<<"No\n"; 
+    }
+}
+signed main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+    freopen("inputf.in" , "r" , stdin ); 
+    freopen("outputf.out", "w" , stdout); 
+    freopen("err.txt","w",stderr);
+    #endif
+    clock_t start, end;
+    start = clock();
+    int t;
+    //cin >> t;
+    t=1;
+    while (t--)
+    {
+        solve();
+    }
+    end = clock();
+    double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
+    cerr << "Time taken by program is : " << fixed 
+         << time_taken << setprecision(5);
+    cerr << " sec " << endl;
+    return 0;
+}
+
